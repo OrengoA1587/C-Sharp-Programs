365842ae43c1f40112e3d46aae84f9a7f705c561
orengoa0459 <102257741+OrengoA1587@users.noreply.github.com>
2022-08-07 19:13:02 -0400

    Moved Files

---
+++ PersonnelForm/WinformUI/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace WinformUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fName_label = new System.Windows.Forms.Label();
+            this.lName_label = new System.Windows.Forms.Label();
+            this.phoneNumber_label = new System.Windows.Forms.Label();
+            this.address_label = new System.Windows.Forms.Label();
+            this.city_label = new System.Windows.Forms.Label();
+            this.state_label = new System.Windows.Forms.Label();
+            this.zipcode_label = new System.Windows.Forms.Label();
+            this.email_label = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // fName_label
+            // 
+            this.fName_label.AutoSize = true;
+            this.fName_label.Location = new System.Drawing.Point(74, 96);
+            this.fName_label.Name = "fName_label";
+            this.fName_label.Size = new System.Drawing.Size(54, 13);
+            this.fName_label.TabIndex = 0;
+            this.fName_label.Text = "FirstName";
+            // 
+            // lName_label
+            // 
+            this.lName_label.AutoSize = true;
+            this.lName_label.Location = new System.Drawing.Point(74, 133);
+            this.lName_label.Name = "lName_label";
+            this.lName_label.Size = new System.Drawing.Size(58, 13);
+            this.lName_label.TabIndex = 1;
+            this.lName_label.Text = "Last Name";
+            // 
+            // phoneNumber_label
+            // 
+            this.phoneNumber_label.AutoSize = true;
+            this.phoneNumber_label.Location = new System.Drawing.Point(73, 172);
+            this.phoneNumber_label.Name = "phoneNumber_label";
+            this.phoneNumber_label.Size = new System.Drawing.Size(78, 13);
+            this.phoneNumber_label.TabIndex = 2;
+            this.phoneNumber_label.Text = "Phone Number";
+            // 
+            // address_label
+            // 
+            this.address_label.AutoSize = true;
+            this.address_label.Location = new System.Drawing.Point(73, 206);
+            this.address_label.Name = "address_label";
+            this.address_label.Size = new System.Drawing.Size(45, 13);
+            this.address_label.TabIndex = 3;
+            this.address_label.Text = "Address";
+            // 
+            // city_label
+            // 
+            this.city_label.AutoSize = true;
+            this.city_label.Location = new System.Drawing.Point(74, 244);
+            this.city_label.Name = "city_label";
+            this.city_label.Size = new System.Drawing.Size(24, 13);
+            this.city_label.TabIndex = 4;
+            this.city_label.Text = "City";
+            // 
+            // state_label
+            // 
+            this.state_label.AutoSize = true;
+            this.state_label.Location = new System.Drawing.Point(73, 274);
+            this.state_label.Name = "state_label";
+            this.state_label.Size = new System.Drawing.Size(32, 13);
+            this.state_label.TabIndex = 5;
+            this.state_label.Text = "State";
+            // 
+            // zipcode_label
+            // 
+            this.zipcode_label.AutoSize = true;
+            this.zipcode_label.Location = new System.Drawing.Point(73, 307);
+            this.zipcode_label.Name = "zipcode_label";
+            this.zipcode_label.Size = new System.Drawing.Size(46, 13);
+            this.zipcode_label.TabIndex = 6;
+            this.zipcode_label.Text = "Zipcode";
+            // 
+            // email_label
+            // 
+            this.email_label.AutoSize = true;
+            this.email_label.Location = new System.Drawing.Point(74, 351);
+            this.email_label.Name = "email_label";
+            this.email_label.Size = new System.Drawing.Size(32, 13);
+            this.email_label.TabIndex = 7;
+            this.email_label.Text = "Email";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(182, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(182, 133);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(183, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(182, 172);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(183, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(182, 203);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(183, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(182, 244);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(183, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(182, 274);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(183, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.email_label);
+            this.Controls.Add(this.zipcode_label);
+            this.Controls.Add(this.state_label);
+            this.Controls.Add(this.city_label);
+            this.Controls.Add(this.address_label);
+            this.Controls.Add(this.phoneNumber_label);
+            this.Controls.Add(this.lName_label);
+            this.Controls.Add(this.fName_label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fName_label;
+        private System.Windows.Forms.Label lName_label;
+        private System.Windows.Forms.Label phoneNumber_label;
+        private System.Windows.Forms.Label address_label;
+        private System.Windows.Forms.Label city_label;
+        private System.Windows.Forms.Label state_label;
+        private System.Windows.Forms.Label zipcode_label;
+        private System.Windows.Forms.Label email_label;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
